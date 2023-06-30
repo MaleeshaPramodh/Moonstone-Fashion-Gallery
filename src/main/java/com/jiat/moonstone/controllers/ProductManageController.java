02e3b55ef674c706b2623c8c5387c37b653502a4
MaleeshaPramodh <maleesha.pramodh.mph@gmail.com>
2023-06-30 10:50:58 +0530

    Moonstone Fashion Gallery updated

--- src/main/java/com/jiat/moonstone/controllers/ProductManageController.java
+++ src/main/java/com/jiat/moonstone/controllers/ProductManageController.java
@@ -10,24 +10,33 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Statement;
 import java.util.List;
 
 @Routes(value = "/productmanage")
 public class ProductManageController {
     Session session = HibernateUtil.getSessionFactory().openSession();
+
     @Route
-    public String get(HttpServletRequest request){
+    public String get(HttpServletRequest request) {
         Query query = session.createQuery("select it from Product it");
-        try{
+        try {
             List<Product> products = query.list();
             request.setAttribute("products", products);
             System.out.println(products);
-        }catch (NoResultException e){
+        } catch (NoResultException e) {
             System.out.println("no result");
         }
         return "adminpanel/productmanage.jsp";
     }
+
     @Route(value = "/add-products", respondsToMethods = {HttpMethod.POST})
     public String addItems(HttpServletRequest request) {
         Session session = HibernateUtil.getSessionFactory().openSession();
@@ -49,4 +58,44 @@ public class ProductManageController {
 
         return "redirect:/productmanage";
     }
+
+    @Route(value = "/delete-products", respondsToMethods = {HttpMethod.POST})
+    public String doPost(HttpServletRequest request) {
+        Query query = session.createQuery("delete from Product where id=:id");
+        try {
+            List<Product> products = query.list();
+            request.setAttribute("id", products);
+            System.out.println(products);
+        } catch (NoResultException e) {
+            System.out.println("no result");
+        }
+        return "adminpanel/productmanage.jsp";
+    }
+
+    @Route(value = "/update-products", respondsToMethods = {HttpMethod.POST})
+    public String update(HttpServletRequest request) {
+
+
+        Query query = session.createQuery("UPDATE Product SET pname=:pname, code=:code, qty=:qty, size=:size, price=:price, description=:description, pimg=:pimg WHERE id=:id");
+
+        query.setParameter("id", 4L);
+
+        Transaction transaction = session.beginTransaction();
+
+        query.executeUpdate();
+
+        transaction.commit();
+
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Product> cqu =  cb.createQuery(Product.class);
+        Root<Product> root = cqu.from(Product.class);
+        cqu.select(root);
+
+        List<Product> list =  session.createQuery(cqu).list();
+        list.forEach(user -> {
+//          System.out.println(user.getEmail());
+        });
+        return "adminpanel/productmanage.jsp";
+    }
+
 }
